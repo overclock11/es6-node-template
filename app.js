f822b6042009f0ae77b8bf9e342aca52fa5597b5
Julian Borray <julian.borray@bizagi.com>
2019-08-22 22:17:09 -0500

    quitar algunas dependencias sobrantes

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const debug = require('debug')('api:server');
 const http = require('http');
-const config = require('./app.config');
-//importar modulos
+const config = require('./config');
 const Hotels =  require('./modules/hotels/HotelsBoot');
+
 
 let app = express();
 let port = config.port;
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 // rewrite virtual urls to angular app to enable refreshing of internal pages
 app.get('/almundo', function (req, res, next) {
     res.sendFile(path.resolve('public/almundo/index.html'));
 });
 
-//importar modulos
+
+
+
 const hotels = new Hotels();
 
-//configurar rutas del modulo
 app.use('/api',hotels.setup());
 
 // levantar servidor express
@@ -41,34 +37,10 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    let bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+    console.log(error);
 }
 function onListening() {
-    let addr = server.address();
-    let bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    console.log('Escuchando en el puerto' + bind);
+    console.log('Escuchando en el puerto' + config.port);
 }
 
 module.exports = app;
